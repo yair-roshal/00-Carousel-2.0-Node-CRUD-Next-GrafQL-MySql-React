a6885185ecfe7066a01dbcc0aceea51109cc2d8d
yair-roshal <yair7705778@gmail.com>
2022-10-16 15:31:02 +0200

    delete 1 file and update 6 files

--- client/src/components/WriterItem.jsx
+++ client/src/components/WriterItem.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import { userAgentMobile } from 'utils';
-import { Link } from 'react-router-dom';
-import Button from '@mui/material/Button';
-import axios from 'axios';
+import React from 'react'
+import { userAgentMobile } from '../utils'
+import { Link } from 'react-router-dom'
+import Button from '@mui/material/Button'
+import axios from 'axios'
 
 export const WriterItem = ({ writer, isButtons }) => {
-	const { id, name, image, article1, article2, article3 } = writer;
+	const { id, name, image, article } = writer
 
 	const deleteClient = () => {
 		axios
 			.delete('http://localhost:5000/' + id)
 			.then(res => {
 				if (res.status === 200) {
-					alert('Client successfully deleted');
-					window.location.reload();
-				} else Promise.reject();
+					alert('Client successfully deleted')
+					window.location.reload()
+				} else Promise.reject()
 			})
-			.catch(err => alert('Something went wrong'));
-	};
+			.catch(err => alert('Something went wrong'))
+	}
 
 	return (
 		<div className='wrapperWriter'>
@@ -28,17 +28,17 @@ export const WriterItem = ({ writer, isButtons }) => {
 
 			<div className='wrapperInfo'>
 				{!userAgentMobile() ? (
-					<div className='blockArticleOne'> {article1} </div>
+					<div className='blockArticleOne'> {article} </div>
 				) : (
 					<div className='blockArticleOne' style={{ 'font-weight': '400 ' }}>
-						{article1}
+						{article}
 					</div>
 				)}
 				{!userAgentMobile() ? (
-					<div className='blockArticleTwoAndThree'> {article2} </div>
+					<div className='blockArticleTwoAndThree'> {article} </div>
 				) : null}
 				{!userAgentMobile() ? (
-					<div className='blockArticleTwoAndThree'> {article3} </div>
+					<div className='blockArticleTwoAndThree'> {article} </div>
 				) : null}
 			</div>
 
@@ -61,5 +61,5 @@ export const WriterItem = ({ writer, isButtons }) => {
 				</div>
 			)}
 		</div>
-	);
-};
+	)
+}
